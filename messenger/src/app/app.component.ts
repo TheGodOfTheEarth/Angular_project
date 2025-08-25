cc65bcf03ecf138702cbad21d5c97a83083691c6
vadzhazha <bura24064@gmail.com>
2025-08-25 19:43:43 +0300

    created the project, added Profilec,service, Profile.interface and added styles for it`s display

---
+++ messenger/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { ProfileService } from './data/services/profile.service';
+import { Profile } from './data/interfaces/profile.interface';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ProfileCardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val) => {
+      this.profiles = val;
+    });
+  }
+}

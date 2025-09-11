296e7ffaa91ee74247c79560a28c3f68912d6382
vadzhazha <bura24064@gmail.com>
2025-09-11 18:43:42 +0300

    RxJS lib. need to update angular to use cookies. made login page necessery to fill and pass

--- messenger/src/app/pages/login-page/login-page.component.ts
+++ messenger/src/app/pages/login-page/login-page.component.ts
@@ -6,7 +6,8 @@ import {
   Validators,
 } from '@angular/forms';
 import { AuthService } from '../../auth/auth.service';
-import { from, map, take } from 'rxjs';
+import { Router } from '@angular/router';
+// import { from, map, take } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
@@ -16,16 +17,29 @@ import { from, map, take } from 'rxjs';
 })
 export class LoginPageComponent {
   authService = inject(AuthService);
+  router = inject(Router);
 
   form = new FormGroup({
-    username: new FormControl(null, Validators.required),
-    password: new FormControl(null, Validators.required),
+    username: new FormControl<string | null>(null, Validators.required),
+    password: new FormControl<string | null>(null, Validators.required),
   });
+
+  //   constructor() {
+  //     from([1, 2, 3, 4, 5, 6, 7, 8, 9])
+  //       .pipe(
+  //         map((val: number) => val * 2),
+  //         take(2)
+  //       )
+  //       .subscribe((val) => {
+  //         console.log(val);
+  //       });
+  //   }     библиотека RxJS нужна для реактивного программирования в JavaScript. Она позволяет разработчикам писать асинхронный код, основанный на событиях, с использованием наблюдаемых значений (Observables). Это упрощает управление потоками данных, реакцию на взаимодействие пользователя и обработку сетевых запросов.
 
   onSubmit() {
     if (this.form.valid) {
       //@ts-ignore
       this.authService.login(this.form.value).subscribe((res) => {
+        this.router.navigate(['']);
         console.log(res);
       });
     }

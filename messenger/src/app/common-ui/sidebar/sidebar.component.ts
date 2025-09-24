5a67d97d4628d833566b0b6142ded19c317387b2
vadzhazha <bura24064@gmail.com>
2025-09-24 19:48:07 +0300

    feat (sidebar): add subscribers list

--- messenger/src/app/common-ui/sidebar/sidebar.component.ts
+++ messenger/src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { SubscriberCardComponent } from './subscriber-card/subscriber-card.component';
+import { RouterLink } from '@angular/router';
+import { ProfileService } from '../../data/services/profile.service';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SvgIconComponent, CommonModule],
+  imports: [
+    SvgIconComponent,
+    CommonModule,
+    SubscriberCardComponent,
+    RouterLink,
+    AsyncPipe,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  profileService = inject(ProfileService);
+
+  subscribers$ = this.profileService.getSubscribersShortList();
+
+  me = this.profileService.me;
+
   menuItems = [
     {
       label: 'Моя страница',

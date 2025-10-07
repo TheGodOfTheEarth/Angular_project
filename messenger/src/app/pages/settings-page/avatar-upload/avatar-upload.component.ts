60a37674375b92aac61299cb3af9c98fd009c4d9
vadzhazha <bura24064@gmail.com>
2025-10-07 10:05:44 +0300

    feat (settings): add DnD directive

--- messenger/src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
+++ messenger/src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
@@ -1,17 +1,28 @@
 import { Component, signal } from '@angular/core';
+import { DndDirective } from '../../../common-ui/directives/dnd.directive';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-avatar-upload',
-  imports: [],
+  imports: [DndDirective, FormsModule],
   templateUrl: './avatar-upload.component.html',
   styleUrl: './avatar-upload.component.scss',
 })
 export class AvatarUploadComponent {
   preview = signal<string>('/assets/images/avatar.jpg');
 
+  avatar = null;
+
   fileBrowserHandler(event: Event) {
     const file = (event.target as HTMLInputElement)?.files?.[0];
+    this.processFile(file);
+  }
 
+  onFileDropped(file: File) {
+    this.processFile(file);
+  }
+
+  processFile(file: File | null | undefined) {
     if (!file || !file.type.match('image')) return;
 
     const reader = new FileReader();
